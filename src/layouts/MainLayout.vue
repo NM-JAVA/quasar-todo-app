21ccf547b21a0d3eb2ba726ce125f5b662e9aa24
NM-JAVA <nurmax1993@gmail.com>
2020-09-11 00:18:37 +0500

    Ok

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,107 +1,154 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+    <q-layout view="lHh Lpr lFf">
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+        <q-header elevated>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+            <q-toolbar>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+                <q-btn flat dense round icon="menu" aria-label="Menu"
+                       @click="leftDrawerOpen = !leftDrawerOpen"/>
+
+            </q-toolbar>
+            <div class="q-px-lg q-pt-xl q-mb-md">
+                <div class="text-h3">Todo</div>
+                <div class="text-subtitle">{{todayDays }}</div>
+            </div>
+
+            <q-img alt="Header logo" src="~assets/header.jpg" class="header-image absolute-top"/>
+
+        </q-header>
+
+        <q-drawer v-model="leftDrawerOpen" show-if-above :width="250" :breakpoint="600">
+
+            <q-img class="absolute-top" src="~assets/header.jpg" style="height: 185px">
+                <div class="absolute-bottom bg-transparent">
+                    <q-avatar size="56px" class="q-mb-sm">
+                        <img src="https://lh3.googleusercontent.com/ogw/ADGmqu_qxYQ-g3nRymKRM3GE6tA5iuQ7rbjfT89V-8Xi=s32-c-mo">
+                    </q-avatar>
+                    <div class="text-weight-bold">Nurbek Makhmudov</div>
+                    <div>@NurbekMakhmudov</div>
+                </div>
+            </q-img>
+
+
+            <q-scroll-area style="height: calc(100% - 185px); margin-top: 185px; border-right: 1px solid #ddd">
+                <q-list padding>
+                    <q-item exact to="/" clickable v-ripple>
+
+                        <q-item-section avatar>
+                            <q-icon name="list"/>
+                        </q-item-section>
+
+                        <q-item-section>
+                            Todo
+                        </q-item-section>
+
+                    </q-item>
+
+                    <q-item exact to="/help" clickable v-ripple>
+
+                        <q-item-section avatar>
+                            <q-icon name="help"/>
+                        </q-item-section>
+
+                        <q-item-section>
+                            Help
+                        </q-item-section>
+
+                    </q-item>
+
+
+                </q-list>
+            </q-scroll-area>
+
+
+        </q-drawer>
+
+
+        <q-page-container>
+
+
+
+            <router-view/>
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+    import {date} from 'quasar'
+    import EssentialLink from 'components/EssentialLink.vue'
 
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+    const linksData = [
+        {
+            title: 'Docs',
+            caption: 'quasar.dev',
+            icon: 'school',
+            link: 'https://quasar.dev'
+        },
+        {
+            title: 'Github',
+            caption: 'github.com/quasarframework',
+            icon: 'code',
+            link: 'https://github.com/quasarframework'
+        },
+        {
+            title: 'Discord Chat Channel',
+            caption: 'chat.quasar.dev',
+            icon: 'chat',
+            link: 'https://chat.quasar.dev'
+        },
+        {
+            title: 'Forum',
+            caption: 'forum.quasar.dev',
+            icon: 'record_voice_over',
+            link: 'https://forum.quasar.dev'
+        },
+        {
+            title: 'Twitter',
+            caption: '@quasarframework',
+            icon: 'rss_feed',
+            link: 'https://twitter.quasar.dev'
+        },
+        {
+            title: 'Facebook',
+            caption: '@QuasarFramework',
+            icon: 'public',
+            link: 'https://facebook.quasar.dev'
+        },
+        {
+            title: 'Quasar Awesome',
+            caption: 'Community Quasar projects',
+            icon: 'favorite',
+            link: 'https://awesome.quasar.dev'
+        }
+    ];
 
-export default {
-  name: 'MainLayout',
-  components: { EssentialLink },
-  data () {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+    export default {
+        name: 'MainLayout',
+        components: {EssentialLink},
+        data() {
+            return {
+                leftDrawerOpen: false,
+                essentialLinks: linksData
+            }
+        },
+        computed: {
+            todayDays() {
+                let timeStamp = Date.now()
+                return date.formatDate(timeStamp, 'dddd D MMMM')
+            }
+        }
     }
-  }
-}
 </script>
+
+
+<style lang="scss">
+    .header-image {
+        height: 100%;
+        z-index: -1;
+        opacity: 0.2;
+        filter: grayscale(100%);
+    }
+
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi at, culpa delectus maxime nesciunt officia optio pariatur sapiente. Blanditiis commodi consequuntur cum dignissimos esse ipsa maiores modi molestias nihil!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut consequatur consequuntur culpa cum dignissimos distinctio eveniet fugiat hic, minus officiis perferendis quia quis recusandae repudiandae unde veniam veritatis voluptate?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eligendi eos, fuga molestiae odit rem sequi. Aliquid animi at corporis facilis minus mollitia nesciunt, non odit quidem repellat tenetur ut!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam atque cumque eaque enim, harum ipsum nemo nisi placeat reiciendis sed soluta tempore ut. Excepturi, provident qui. Eaque, enim, reprehenderit?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
